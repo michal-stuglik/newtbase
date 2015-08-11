1fffe79e20b4cb1c326464bb5619ed3d31cba893
michal-stuglik <stuglik@gmail.com>
2015-08-11 09:20:47 +0200

    - models tests refactored

--- newtbase/tests/test_models.py
+++ newtbase/tests/test_models.py
@@ -18,50 +18,86 @@ class TranscriptTestMommy(TestCase):
 
 class TranscriptTestMommy(TestCase):
     def setUp(self):
-        self.o = mommy.make(Transcript)
+        self.o = mommy.make(Transcript, sequence="AACC")
 
     def tearDown(self):
         self.o.delete()
 
     def test_transcript(self):
         self.assertEqual(self.o.__unicode__(), self.o.transcript_id)
-        self.assertTrue(self.o.sequence is not None)
+        self.assertIsNotNone(self.o.sequence)
 
-        # def test_sequence_length(self):
-        #     self.assertTrue(len(self.o.sequence) > 0)
+    def test_sequence_length(self):
+        self.assertTrue(self.o.get_sequence_length() == 4)
 
 
 class OrfTestMommy(TestCase):
+
+    def setUp(self):
+        self.o = mommy.make(Orf)
+
+    def tearDown(self):
+        self.o.delete()
+
     def test_orf(self):
-        o = mommy.make(Orf)
-        self.assertEquals(o.__unicode__(), o.orf_id)
-        self.assertTrue(isinstance(o.transcript, Transcript))
+        self.assertEquals(self.o.__unicode__(),self.o.orf_id)
+        self.assertTrue(isinstance(self.o.transcript, Transcript))
 
 
 class AccessionTestMommy(TestCase):
+
+    def setUp(self):
+        self.o = mommy.make(Accession)
+
+    def tearDown(self):
+        self.o.delete()
+
     def test_accession(self):
-        o = mommy.make(Accession)
-        self.assertEquals(o.__unicode__(), o.accession_id)
+        self.assertEquals(self.o.__unicode__(), self.o.accession_id)
 
 
 class BlastTestMommy(TestCase):
+
+    def setUp(self):
+        self.o = mommy.make(Blast, database="Swissprot")
+
+    def tearDown(self):
+        self.o.delete()
+
     def test_blast(self):
-        o = mommy.make(Blast)
-        self.assertTrue(isinstance(o, Blast))
-        self.assertEquals(o.__unicode__(), "{},{},{}".format(o.transcript_id, o.database, o.evalue))
+        self.assertIsInstance(self.o, Blast)
+        self.assertEquals(self.o.__unicode__(), "{},{},{}".format(self.o.transcript_id, self.o.database, self.o.evalue))
+        self.assertTrue(self.o.get_db_url(), "{}{}".format('http://www.uniprot.org/uniprot/', self.o.accession_id))
 
 
 class GoTestMommy(TestCase):
+
+    def setUp(self):
+        self.bd = mommy.make(Go, category="biological_process")
+        self.o = mommy.make(Go)
+
+    def tearDown(self):
+        self.o.delete()
+        self.bd.delete()
+
     def test_go(self):
-        o = mommy.make(Go)
-        self.assertEquals(o.__unicode__(), o.id)
+        self.assertEquals(self.o.__unicode__(), self.o.id)
+        self.assertTrue(self.bd.is_category_biological_process())
+        self.assertFalse(self.bd.is_category_molecular_function())
+        self.assertFalse(self.bd.is_category_cellular_component())
 
 
 class GoUniprotMapperTestMommy(TestCase):
+
+    def setUp(self):
+        self.o = mommy.make(GoUniprotMapper)
+
+    def tearDown(self):
+        self.o.delete()
+
     def test_gouniprot(self):
-        o = mommy.make(GoUniprotMapper)
-        self.assertTrue(isinstance(o, GoUniprotMapper))
+        self.assertTrue(isinstance(self.o, GoUniprotMapper))
 
 
-if __name__ == '__main__':
-    test.main()
+# if __name__ == '__main__':
+#     test.main()
