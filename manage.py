37b23c21b3ae8975feaa04ed8db55b48f4e3c3ac
Michal Stuglik <stuglik@gmail.com>
2018-07-16 14:46:28 +0200

    Revert back default setting

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newtbase.settings.default")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newtbase.settings.prod")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newtbase.settings.default")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newtbase.settings.prod")
 
     from django.core.management import execute_from_command_line
 
